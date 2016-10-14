3645f7ebd3c00d926b62229ceefe79ff5be92127
juliamschultz <schultzj@gilmour.org>
2016-10-14 09:15:39 -0400

    dance

--- student.py
+++ student.py
@@ -77,7 +77,7 @@ class GoPiggy(pigo.Pigo):
             self.encR(16)
             self.encL(10)
             if self.isClear():
-                self.encF(10)
+                self.encF(20)
                 right_rot()
                 time.sleep(1)
                 left_rot()
