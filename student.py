da2f7e8f1c1b58bf1c44d3e24307b3cd799a796a
juliamschultz <schultzj@gilmour.org>
2016-11-07 10:29:26 -0500

    changing back to choosepath

--- student.py
+++ student.py
@@ -112,7 +112,7 @@ class GoPiggy(pigo.Pigo):
                 print ("something is in my way")
             #checking for alternate routes
             #TODO figure out why its doesnt drive until there is something in the way
-            answer = self.chooseBetter()
+            answer = self.choosePath()
             #TODO try and make it so it wont go in the opposite direction that it is supposed to (basically make it not turn right or left for than two times in a row)
             #TODO figure out why it turns when it can clearly go straight
             print ("I found a new path!")
