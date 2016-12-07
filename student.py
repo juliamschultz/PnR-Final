b5c7c9b653d0242dd0610a80a62ef05e6f5e1265
juliamschultz <schultzj@gilmour.org>
2016-12-07 08:52:17 -0500

    fixing turns to make them more accurately

--- student.py
+++ student.py
@@ -19,7 +19,7 @@ class GoPiggy(pigo.Pigo):
     LEFT_SPEED = 113
 
     turn_track = 0.0
-    TIME_PER_DEGREE = .012
+    TIME_PER_DEGREE = .0112
     TURN_MODIFIER = .75
 
     def setSpeed(self, l, r):
